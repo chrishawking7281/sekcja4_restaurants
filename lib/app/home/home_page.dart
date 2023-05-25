abe05ad6077916bddec197e0cd1a6d580d52a4c2
Krzysztof Kochel <k.kochel7281@gmail.com>
2023-05-25 17:14:07 +0200

    Add bottom navigation bar to HomePage

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({
     super.key,
     required this.user,
@@ -10,23 +10,63 @@ class HomePage extends StatelessWidget {
   final User user;
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+var currentIndex = 0;
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Jesteś zalogowany jako ${user.email}'),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              child: const Text('Wyloguj się'),
+      body: Builder(builder: (context) {
+        if (currentIndex == 0) {
+          return const Center(
+            child: Text('1'),
+          );
+        }
+        if (currentIndex == 1) {
+          return const Center(
+            child: Text('2'),
+          );
+        }
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Jesteś zalogowany jako ${widget.user.email}'),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+                child: const Text('Wyloguj się'),
+              ),
+            ],
+          ),
+        );
+      }),
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: (newIndex) {
+            setState(() {
+              currentIndex = newIndex;
+            });
+          },
+          currentIndex: currentIndex,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.reviews),
+              label: 'Krytyka',
             ),
-          ],
-        ),
-      ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add),
+              label: 'Utwórz',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Moje konto',
+            ),
+          ]),
     );
   }
 }
