a8c7fa5534825367afebcc72a128c29801b7c6cf
Krzysztof Kochel <k.kochel7281@gmail.com>
2023-05-27 16:07:46 +0200

    Add container and dismissible

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class AddOpinionPageContent extends StatelessWidget {
@@ -8,41 +7,8 @@ class AddOpinionPageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('restaurant').snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const Center(child: Text('Wystąpił nieoczekiwany błąd'));
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-                child: Text('Ładowanie danych trwa dłużej niż powinno'));
-          }
-          final documents = snapshot.data!.docs;
-          return ListView(
-            children: [
-              for (final document in documents) ...[
-                Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(document['name']),
-                          Text(document['dish']),
-                        ],
-                      ),
-                      Text(
-                        document['rating'].toString(),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ],
-          );
-        });
+    return const Center(
+      child: Text('1'),
+    );
   }
 }
--- lib/app/home/restaurants/restaurants_page_content.dart
+++ lib/app/home/restaurants/restaurants_page_content.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class RestaurantPageContent extends StatelessWidget {
@@ -7,8 +8,56 @@ class RestaurantPageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('1'),
-    );
+    return StreamBuilder<QuerySnapshot>(
+        stream:
+            FirebaseFirestore.instance.collection('restaurants').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(child: Text('Wystąpił nieoczekiwany błąd'));
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+                child: Text('Ładowanie danych trwa dłużej niż powinno'));
+          }
+          final documents = snapshot.data!.docs;
+          return ListView(
+            children: [
+              for (final document in documents) ...[
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Dismissible(
+                    key: ValueKey(document.id),
+                    onDismissed: (_) {
+                      FirebaseFirestore.instance
+                          .collection('restaurants')
+                          .doc(document.id)
+                          .delete();
+                    },
+                    child: Container(
+                      color: Colors.lightBlueAccent,
+                      padding: const EdgeInsets.all(10),
+                      margin: const EdgeInsets.all(5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(document['name']),
+                              Text(document['dish']),
+                            ],
+                          ),
+                          Text(
+                            document['rating'].toString(),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ],
+          );
+        });
   }
 }
